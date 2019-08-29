db3a7a4a4d96b614945260e16d5c8db44de54675
Ahmed Shahein <44742387+ahmedshahein@users.noreply.github.com>
2019-08-29 07:54:33 +0200

    Update nco.m
    Added cos output

--- .drl_src_code/sgen_nco/octave/nco.m
+++ .drl_src_code/sgen_nco/octave/nco.m
@@ -1,4 +1,4 @@
-function y2 = nco(param)
+function [osin, ocos] = nco(param)
 % The NCO model is developed by Kadhiem Ayob and it is posted at dsprelated!
 % https://www.dsprelated.com/showcode/228.php
 % Design Parameters
@@ -11,18 +11,16 @@ fcw   = floor(M*fo/fs);                 %NCO accumulator phase fcwrement
 k     = 2^param.gp_rom_depth;           %lut phase resolution (lut size), may equal M
 
 % Single equation for NCO bit true model
-lsb   = log2(M) - log2(k);              %LSBs discarded when addressing
-lut   = round(A*sin(2*pi*(0:k-1)/k));   %lut, one cycle sine data     %lut, one cycle sine data
-
-r_min = -10000;
-r_max =  10000;
-r = r_min + (r_max - r_min)*rand(1,n);
+lsb       = log2(M) - log2(k);              %LSBs discarded when addressing
+lut_sin   = round(A*sin(2*pi*(0:k-1)/k));   %lut, one cycle sine data     %lut, one cycle sine data
+lut_cos   = round(A*cos(2*pi*(0:k-1)/k));
 
 ptr   = 0; 
 addr  = 0;
 for i = 1:n 
-    y2(i)           = lut(addr+1);          %add 1 for matlab LUT
-    ptr             = mod(ptr + fcw, M)+r(i);    %phase accumulator(ramp)
+    osin(i)         = lut_sin(addr+1);          %add 1 for matlab LUT
+    ocos(i)         = lut_cos(addr+1); 
+    ptr             = mod(ptr + fcw, M);    %phase accumulator(ramp)
     addr            = floor(ptr/2^lsb);     %discard LSBs %%round
     addr(addr >= k) = addr - k;             %check address overflow
 end
