4a0711e62325faaa5df25862d925deb3965a4565
Ahmed Shahein <44742387+ahmedshahein@users.noreply.github.com>
2019-12-01 22:35:53 +0100

    Update gen_defines.m
    Rename the prefix to gp_ instead of p_ to match the RTL.

--- .drl_src_code/filt_ppd/octave/gen_defines.m
+++ .drl_src_code/filt_ppd/octave/gen_defines.m
@@ -4,17 +4,17 @@ function gen_defines(defines)
   
   fprintf(fid, "`define DIV(N, D) (N%%D==0) ? (N/D) : (N/D+1)\n");
   fprintf(fid,"\n");
-  fprintf(fid, "`define P_INP_DATA_W \t%d\n",defines.p_data_width);
-  fprintf(fid, "`define P_DECIMATION \t%d\n",defines.p_decimation_factor);
-  fprintf(fid, "`define P_COEFF_L \t%d\n",defines.p_coeff_length);
-  fprintf(fid, "`define P_COEFF_W \t%d\n",defines.p_coeff_width);
-  fprintf(fid, "`define P_TF_DF \t%d\n",defines.p_tf_df);
-  fprintf(fid, "`define P_COMM_R_OUP \t%d\n",defines.p_comm_reg_oup);
-  fprintf(fid, "`define P_COMM_CCW_CW \t%d\n",defines.p_comm_ccw);
-  fprintf(fid, "`define P_MUL_CCW_CW \t%d\n",defines.p_mul_ccw);
-  fprintf(fid, "`define P_COMM_PHA \t%d\n",defines.p_comm_phase);
+  fprintf(fid, "`define P_INP_DATA_W \t%d\n",defines.gp_idata_width);
+  fprintf(fid, "`define P_DECIMATION \t%d\n",defines.gp_decimation_factor);
+  fprintf(fid, "`define P_COEFF_L \t%d\n",defines.gp_coeff_length);
+  fprintf(fid, "`define P_COEFF_W \t%d\n",defines.gp_coeff_width);
+  fprintf(fid, "`define P_TF_DF \t%d\n",defines.gp_tf_df);
+  fprintf(fid, "`define P_COMM_R_OUP \t%d\n",defines.gp_comm_reg_oup);
+  fprintf(fid, "`define P_COMM_CCW_CW \t%d\n",defines.gp_comm_ccw);
+  fprintf(fid, "`define P_MUL_CCW_CW \t%d\n",defines.gp_mul_ccw);
+  fprintf(fid, "`define P_COMM_PHA \t%d\n",defines.gp_comm_phase);
   %fprintf(fid, "`define P_OUP_W     \t(`P_INP_DATA_W+`P_COEFF_W+`P_DECIMATION+(`DIV(`P_COEFF_L,`P_DECIMATION)))\n");
-  fprintf(fid, "`define P_OUP_W     \t%d\n", defines.p_data_width+defines.p_coeff_width+ceil(log2(defines.p_decimation_factor))+ceil(log2(defines.p_coeff_length/defines.p_decimation_factor)));
+  fprintf(fid, "`define P_OUP_W     \t%d\n", defines.gp_idata_width+defines.gp_coeff_width+ceil(log2(defines.gp_decimation_factor))+ceil(log2(defines.gp_coeff_length/defines.gp_decimation_factor)));
   fprintf(fid, "\n");
   fprintf(fid, "`define TESTCASE\t%d\n",defines.testcase);
   fprintf(fid, "\n");
